5fcc932dac431a9c15f72b660d2a75e8831bba2a
Shane Padgett <contact@shanepadgett.dev>
2025-04-08 15:10:36 -0400

    ios app init

---
+++ ios/featureFlags/featureFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  featureFlagsApp.swift
+//  featureFlags
+//
+//  Created by Shane Padgett on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct featureFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
